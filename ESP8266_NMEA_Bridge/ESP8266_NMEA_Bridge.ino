c62d9f49160e0d288dabaa330c681500b6ce2ae8
pi3412 <36189606+pi3412@users.noreply.github.com>
2018-03-22 15:55:14 +0100

    Update ESP8266_NMEA_Bridge.ino

--- ESP8266_NMEA_Bridge/ESP8266_NMEA_Bridge.ino
+++ ESP8266_NMEA_Bridge/ESP8266_NMEA_Bridge.ino
@@ -27,8 +27,10 @@ int charCount(0);
 int readCh(0);
 byte parityIn(0);
 byte parityOut(0);
-char headerText[NMEA_HEADER_LENGTH];
-char parityText[NMEA_PARITY_LENGTH];
+char headerText[NMEA_HEADER_LENGTH]; // No string, just a character array
+char parityText[NMEA_PARITY_LENGTH]; // No string, just a character array
+char outputText[NMEA_MAX_LENGTH]; // A string
+// char udpText[NMEA_MAX_LENGTH]; // A string
 enum TypSentencePart { Header, Body, Parity, Other };
 TypSentencePart SentencePart (Other);
 
@@ -193,6 +195,7 @@ void loop() {
           if (usableSentence) {
             Serial1.printf("$GP");
             udp.printf("$GP");
+ //           outputText;
             Serial1.print(HeaderText[2]);
             udp.print(HeaderText[2]);
             Serial1.print(HeaderText[3]);
@@ -262,3 +265,50 @@ void loop() {
     }
   }
 }
+
+/* Process Header
+
+void processHeader (TypSentencePart* locSP, int* locChar, int* locCC, string* locTXT)
+        if (locCC < NMEA_HEADER_LENGTH)  { // Header not yet complete
+          headerText[charCount++] = readCh;
+          switch (charCount) {
+            case 1:
+              parityIn = (byte)readCh;
+              parityOut = 'G';
+              break;
+            case 2:
+              parityIn ^= (byte)readCh;
+              parityOut ^= 'P';
+              break;
+            default:
+              parityIn ^= (byte)readCh;
+              parityOut ^= (byte)readCh;
+              break;
+          }
+        }
+        else { // Header complete
+          HeaderText[charCount++] = 0;
+          usableSentence = 0;
+          if ((HeaderText[2] == 'G') && (HeaderText[3] == 'G') && (HeaderText[4] == 'A')) usableSentence = 1; // If header is xxGGA
+          if ((HeaderText[2] == 'R') && (HeaderText[3] == 'M') && (HeaderText[4] == 'C')) usableSentence = 1; // If header is xxRMC
+          charCount = 0;
+          if (usableSentence) {
+            Serial1.printf("$GP");
+            udp.printf("$GP");
+ //           outputText;
+            Serial1.print(HeaderText[2]);
+            udp.print(HeaderText[2]);
+            Serial1.print(HeaderText[3]);
+            udp.print(HeaderText[3]);
+            Serial1.print(HeaderText[4]);
+            udp.print(HeaderText[4]);
+            // HeaderText[charCount++] = readCh; // The character (,) of this loop must be read, otherwise it is lost
+            Serial1.printf(",");
+            udp.printf(",");
+            parityIn ^= (byte)readCh;
+            parityOut ^= (byte)readCh;
+            SentencePart = Body;
+          }
+          else SentencePart = Other;
+        }
+*/
